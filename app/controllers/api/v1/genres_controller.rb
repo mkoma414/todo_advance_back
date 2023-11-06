4e31be30af247895f4dc37b1f2f2fe49f0724ff7
Komatsubara Masato <m-koma@nifty.com>
2023-11-06 15:11:16 +0900

    namespaceの追加

---
+++ app/controllers/api/v1/genres_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class GenresController < ApplicationController
+      before_action :select_genre, only: [:destroy]
+      skip_before_action :verify_authenticity_token
+      
+      def index
+        genres_all
+      end
+
+      def create
+        Genre.create(genre_params)
+        genres_all
+      end
+
+      def destroy
+        @genre.destroy
+        genres_all
+      end
+
+      private
+
+      def genre_params
+        params.require(:genre).permit(:name)
+      end
+
+      def select_genre
+        @genre = Genre.find(params[:id])
+      end
+
+      def genres_all
+        genres = Genre.all
+        render json: genres
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :tasks , defaults: {format: 'json'} do
-    member do
-      post :status, to: "tasks#update_status", defaults: {format: 'json'}
+  namespace :api do
+    namespace :v1 do
+      resources :tasks , defaults: {format: 'json'} do
+        member do
+          post :status, to: "tasks#update_status", defaults: {format: 'json'}
+        end
+      end
+      resources :genres
     end
   end
-  resources :genres
 end
